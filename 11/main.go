ac92df30edd2365fc1565698fe05c8ed3bb171d4
hw <hussainweb@gmail.com>
2024-12-11 23:29:02 -0500

    Day 11 - Part 2

--- 11/main.go
+++ 11/main.go
@@ -13,25 +13,58 @@ func panicErr(e error) {
 	}
 }
 
+var stoneCache map[string]int = make(map[string]int)
+
 func main() {
 	dat, err := os.ReadFile("input.txt")
 	panicErr(err)
 
 	var stones []uint64
-	for _, stoneStr := range strings.Split(strings.Trim(string(dat), "\n"), " ") {
+	for _, stoneStr := range strings.Split(strings.Trim(string(dat), "\n "), " ") {
 		i, e := strconv.ParseUint(stoneStr, 10, 64)
 		panicErr(e)
 		stones = append(stones, i)
 	}
 
 	fmt.Println(stones)
-	for i := 0; i < 25; i++ {
-		stones = blink(stones)
-		fmt.Println("Iteration", i, "Number of stones", len(stones))
-		// fmt.Println(stones)
+
+	sum := 0
+	for _, s := range stones {
+		sum += blinkRecursive(s, 75)
 	}
 
-	fmt.Println(len(stones))
+	fmt.Println(sum)
+}
+
+func blinkRecursive(stone uint64, depth int) int {
+	key := strconv.FormatUint(stone, 10) + "-" + strconv.Itoa(depth)
+	val, okay := stoneCache[key]
+	if okay {
+		return val
+	}
+
+	if depth == 0 {
+		return 1
+	}
+
+	if stone == 0 {
+		stoneCache[key] = blinkRecursive(1, depth-1)
+		return stoneCache[key]
+	}
+
+	ss := strconv.FormatUint(stone, 10)
+	l := len(ss)
+	if l%2 == 0 {
+		i1, e1 := strconv.ParseUint(ss[0:l/2], 10, 64)
+		panicErr(e1)
+		i2, e2 := strconv.ParseUint(ss[l/2:l], 10, 64)
+		panicErr(e2)
+		stoneCache[key] = blinkRecursive(i1, depth-1) + blinkRecursive(i2, depth-1)
+		return stoneCache[key]
+	}
+
+	stoneCache[key] = blinkRecursive(stone*2024, depth-1)
+	return stoneCache[key]
 }
 
 func blink(stones []uint64) []uint64 {
